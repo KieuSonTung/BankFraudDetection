d7f5163d18659e9bd9e6075bd69baf29330fc1b4
KieuSonTung <sontungkieu412@gmail.com>
2023-10-11 10:02:49 +0700

    Move process func from train.py to utils.py

--- src/model/xgb_train.py
+++ src/model/xgb_train.py
@@ -11,63 +11,7 @@ optuna.logging.set_verbosity(optuna.logging.WARNING)
 warnings.filterwarnings("ignore")
 
 class XGBoostModel:
-
-    def process_cat_feats(self, X, object_cols):
-        ohe = OneHotEncoder(sparse=False, handle_unknown='ignore')
-
-        # Get one-hot-encoded columns
-        ohe_cols = pd.DataFrame(ohe.fit_transform(X[object_cols]))
-        ohe_cols.index = X.index
-
-        num_X = X.drop(object_cols, axis=1)
-        
-        X = pd.concat([num_X, ohe_cols], axis=1)
-
-        X.columns = X.columns.astype(str)
-
-        return X
-    
-    def split_train_test_set(self, df):
-        # Split data into features and target
-        X = df.drop(['fraud_bool'], axis=1)
-        y = df['fraud_bool']
-
-        # Train test split by 'month', month 0-5 are train, 6-7 are test data as proposed in the paper
-        X_train = X[X['month'] < 6]
-        X_test = X[X['month'] >= 6]
-        y_train = y[X['month'] < 6]
-        y_test = y[X['month'] >= 6]
-
-        X_train.drop('month', axis=1, inplace=True)
-        X_test.drop('month', axis=1, inplace=True)
-
-        return X_train, y_train, X_test, y_test
-    
-    def preproces(self, df):
-        df = df.drop(['device_fraud_count'], axis=1, errors='ignore')
-
-        X_train, y_train, X_test, y_test = self.split_train_test_set(df)
-        
-        # list of column-names and whether they contain categorical features
-        s = (X_train.dtypes == 'object') 
-        object_cols = list(s[s].index)
-
-        X_train = self.process_cat_feats(X_train, object_cols)
-        X_test = self.process_cat_feats(X_test, object_cols)
-
-        X_train = X_train.reset_index() \
-                .drop(columns='index')
-        X_test = X_test.reset_index() \
-                .drop(columns='index')
-        
-        y_train = y_train.reset_index() \
-                .drop(columns='index')
-        y_test = y_test.reset_index() \
-                .drop(columns='index')
-
-        return X_train, y_train, X_test, y_test
-        
-    def optimize(self, X_train, y_train, n_trials=1):
+    def optimize(self, X_train, y_train, n_trials=5):
         def objective(trial, data, target):
             train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.2, random_state=42)
 
@@ -84,7 +28,7 @@ class XGBoostModel:
                 'min_child_weight': trial.suggest_int('min_child_samples', 1, 30)
             }
 
-            model = XGBClassifier(**param, tree_method='gpu_hist', verbosity = 0)  
+            model = XGBClassifier(**param, verbosity=0)  
             model.fit(
                 train_x, train_y,
                 eval_set=[(test_x,test_y)],
@@ -105,12 +49,6 @@ class XGBoostModel:
         print('Number of finished trials:', len(study.trials))
         
         return study.best_trial.params
-        
-    def custom_tpr(self, y_true, y_pred):
-        fprs, tprs, thresholds = roc_curve(y_true, y_pred)
-        tpr = tprs[fprs < 0.05][-1]
-        
-        return tpr
     
     def retrain_kfold(self, X_train, y_train, X_test, y_test, best_params, n_splits=5):        
         preds = 0
@@ -119,7 +57,7 @@ class XGBoostModel:
         y_pred_ls, y_prob_ls = [], []
 
         skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
-        model = XGBClassifier(**best_params, tree_method='gpu_hist', verbosity=0, n_jobs=-1)
+        model = XGBClassifier(**best_params, verbosity=0, n_jobs=-1)
         
         for num, (train_idx, valid_idx) in enumerate(skf.split(X_train, y_train)):
             train_x, val_x = X_train.loc[train_idx], X_train.loc[valid_idx]
@@ -156,7 +94,8 @@ class XGBoostModel:
         voting_method: voting method (options: soft, hard; default: soft)
         '''
         
-        X_train, y_train, X_test, y_test = self.preproces(df)
+        p = utils.PreProcess()
+        X_train, y_train, X_test, y_test = p.fit(df)
 
         best_params = self.optimize(X_train, y_train)
 
